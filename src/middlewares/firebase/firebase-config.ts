6f2c4f387d9f098f0dec9804a0ae19bc62f7f518
CondeArmand <neoarmandosousa1010@hotmail.com>
2024-03-25 14:40:26 -0300

    Projeto convertido para type modules.

--- src/middlewares/firebase/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-
-const serviceAccount = require('./ServiceAccount.js');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
